34bb262347d2fa8836e3c2befd9811c9e22aeb46
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-06-07 14:03:18 -0700

    added background image and components

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import backgroundImg from '../background.png';
+
+export default function Main() {
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+        hello from main.js
+    </main>
+  );
+}
